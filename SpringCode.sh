5e9a386c8f9b7d25644edd47ab60367be0e215df
wadanaoumi <wada121400@gmail.com>
2021-10-06 14:04:43 +0900

    シェルスクリプトの設定修正

--- SpringCode.sh
+++ SpringCode.sh
@@ -3,5 +3,5 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i /local/opneapi.yaml \
     -g spring \
-    --additional-properties interfaceOnly
+    --additional-properties interfaceOnly \
     -o /local/springcode
